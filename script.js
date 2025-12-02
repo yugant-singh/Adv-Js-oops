72755b47b6293ffe37991091ee865802e745c6e9
yugant-singh <yugantsingh9651@gmail.com>
2025-12-02 16:33:53 +0530

    solve the some pratice question

---
+++ script.js
@@ -0,0 +1,64 @@
+//✅ // 1. Ek class Person banao jisme name aur age ho.
+// // Object banao aur dono print karo.
+// class Person {
+//     constructor() {
+//         this.name = "Yugant";
+//         this.age = 23;
+//     }
+//     printKro() {
+//         console.log(`My name is ${this.name} and i am ${this.age} years old`);
+//     }
+// }
+// let p1 = new Person();
+// console.log(p1.printKro());
+// //--------------------------------------------------
+// // ✅ 2. Ek class Car banao jisme brand ho.
+// // Ek method showBrand() banao.
+// class Car{
+//     constructor(brand , color){
+//         this.brand = brand;
+//         this.color = color;
+//     }
+
+//     showBrand(){
+//         console.log(`${this.brand} is in ${this.color} color`);
+//     }
+// }
+
+// let c1  = new Car("Tata", "Black");
+// let c2  = new Car("Jaiguar", "Golden");
+// let c3  = new Car("Suzuki", "White");
+//----------------------------------------------------
+
+// // ✅3. Ek class Book banao jisme title, author ho.
+// // Object banao aur book ka title print karo.
+// class Book{
+//     constructor(title,author){
+//         this.title = "Hum hai rahi pyar ke Fir milenge chlte chlte";
+//         this.author = "Yugant Singh";
+//     }
+// }
+
+// let b1  = new Book();
+// console.log(b1.title);
+//----------------------------------------------------
+
+// //✅ 4. Ek class Mobile banao jisme model, price ho.
+// // Ek method showInfo() banao jo dono print kare.
+
+
+// class Mobile{
+//     constructor(model, price){
+//         this.model = model;
+//         this.price = price;
+//     }
+
+//     showInfo(){
+//         console.log(`${this.model}, ${this.price}`);
+//     }
+// }
+
+// let m1  = new Mobile("Samsung", 12000);
+
+
+//--------------------------------------------------
